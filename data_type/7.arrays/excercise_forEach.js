3aef2d709643a1bc1f2697be69f527707084cc96
heijin-yuuby <wajilao214@gmail.com>
2025-03-19 15:13:30 +0800

    more excercises

---
+++ data_type/7.arrays/excercise_forEach.js
@@ -0,0 +1,35 @@
+/* 用forEach添加索引 */    
+var fruits = ["apple", "banana", "cherry", "date", "elderberry"];
+fruits.forEach(function (fruit, index) {
+    console.log(fruit + "在索引[" + index + "]处");
+});
+
+/* 用map生成新数组 */
+var newFruits = fruits.map(function(fruit){
+    return fruit.charAt(0).toUpperCase()+fruit.slice(1);
+
+});
+console.log(newFruits);
+
+/* 用filter筛选特定元素 */
+var longfruits = fruits.filter(function(fruit){
+    return fruit.length>5;
+});
+console.log(longfruits);
+
+/* 用indexOf查找特定元素的索引 */
+var cherryIndex = fruits.indexOf('cherry');
+if(cherryIndex!=-1){
+    var position = cherryIndex + 1;
+    console.log("cherry位置在数组的第："+position+" 个");
+
+}else{
+    console.log("不在该数组中")
+
+
+}
+
+/* 用pop删除最后一个元素 */
+fruits.push('watermelon');
+fruits.pop();
+console.log(fruits)
