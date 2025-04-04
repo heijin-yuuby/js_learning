cf6ec929f4e6311fba706280193647ce668b4eed
heijin-yuuby <wajilao214@gmail.com>
2025-04-04 22:20:03 +0800

    splice

--- java_standard_libruary/Array/instance_method.js
+++ java_standard_libruary/Array/instance_method.js
@@ -19,44 +19,55 @@ var a = ["a", "b", "c"];
 a.shift(); //用于删除数组第一个元素
 
 var list = [1, 2, 3, 4];
-var item;//用于存储从数组中取出的元素
+var item; //用于存储从数组中取出的元素
 
 while ((item = list.shift())) {
-  console.log(item);//打印每次取出的元素
+  console.log(item); //打印每次取出的元素
 }
 
 list; // [],但是这样的方法表明一旦item=0，就会停止遍历
 
-var a = ['a','b','c'];
+var a = ["a", "b", "c"];
 
-a.unshift('x');
+a.unshift("x");
 
-
-var arr = ['c','d'];
-arr.unshift('a','b')//在数组的第一个位置添加元素
-arr//['a','b','c','d']
+var arr = ["c", "d"];
+arr.unshift("a", "b"); //在数组的第一个位置添加元素
+arr; //['a','b','c','d']
 
 /* join(),以指定参数作为分隔符，将所有数组成员链接为一个字符串返回 */
-var a = [1,2,3,4];
+var a = [1, 2, 3, 4];
 
-a.join('')// '1 2 3 4'
-a.join(' | ')//"1 | 2 | 3 | 4"
-a.join()//"1,2,3,4"
+a.join(""); // '1 2 3 4'
+a.join(" | "); //"1 | 2 | 3 | 4"
+a.join() //"1,2,3,4"
 
-/* 数组成员是undefined或null或空，会被转为空字符串 */
-[undefined, null].join('#')
-// "#"
+  [
+    /* 数组成员是undefined或null或空，会被转为空字符串 */
+    (undefined, null)
+  ].join("#")
+  [
+    // "#"
 
-['a','b'].join('-')
+    ("a", "b")
+  ].join("-");
 //'a-b'
 
-Array.prototype.join.call('hello','-')
+Array.prototype.join.call("hello", "-");
 // "h-e-l-l-o"
 
-var obj = {0:'a',1:'b',length: 2 };
-Array.prototype.join.call(obj,'-')
+var obj = { 0: "a", 1: "b", length: 2 };
+Array.prototype.join
+  .call(obj, "-")
 
-
-/* concat()用于多个数组的合并 */
-[1,2,3].concat( 4, 5, 6)
+  [
+    /* concat()用于多个数组的合并 */
+    (1, 2, 3)
+  ].concat(4, 5, 6);
 // [1,2,3,4,5,6]
+
+/* splice()用于删除原数组的一部分成员 */
+arr.splice(StaticRange, CountQueuingStrategy, addElement1, addElement2);
+
+var a = ['a','b','c','d','e','f'];
+a.splice(4,2)//4为从第四位元素开始删除，2为删除两个元素
