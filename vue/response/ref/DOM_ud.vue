3d6a8b42f3ca75c612d96c09956ca881a682a9dc
heijin-yuuby <wajilao214@gmail.com>
2025-04-21 21:34:11 +0800

    update DOM

---
+++ vue/response/ref/DOM_ud.vue
@@ -0,0 +1,21 @@
+<script setup>
+import {nextTick} from 'vue'
+
+const count = ref(0)
+
+async function increment() {
+    count.value++
+    await nextTick()
+    //现在更新了
+    console.log('更新后的count在dom中已经显示');
+    ;
+    
+}
+</script>
+
+<template>
+    <div>
+        <p>{{ count }}</p>
+        <button @click="increment">点击增加count并在DOM更新后执行操作</button>
+    </div>
+</template>
