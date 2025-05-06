df559feb84075e3bb08fb3977a1a2fd4ef20c551
heijin-yuuby <wajilao214@gmail.com>
2025-05-06 23:35:22 +0800

    获取上一次的值

---
+++ vue/calculating/get_last_value.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref,computed } from 'vue'
+
+const count = ref(2)
+const previousCount = ref(count.value);//用于记录上一次的 count
+
+const alwaysSmall = computed(() => {
+    if (count.value <= 3) {
+        previousCount.value = count.value
+        return count.value
+    }
+
+    return previousCount = () => {
+        count.value++;
+    }
+})
+</script>
+
+<template>
+<div>
+    <p>count: {{ count }}</p>
+    <p>alwaysSmall: {{ alwaysSmall }}</p>
+    <button @click="incrementCount">click me</button>
+</div>
+</template>
