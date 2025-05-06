1ca3603a1a9af9f534d90f6e0c18971fab1de819
heijin-yuuby <wajilao214@gmail.com>
2025-05-06 23:14:24 +0800

    writable_property

---
+++ vue/calculating/writable.vue
@@ -0,0 +1,24 @@
+<template>
+    <input v-model="firstName" />
+    <input v-model="lastName" />
+    <p>Full Name: <input v-model="fullName" /></p>
+  </template>
+  
+  <script setup>
+  import { ref, computed } from 'vue';
+  
+  const firstName = ref('John');
+  const lastName = ref('Doe');
+  
+  const fullName = computed({
+    get() {
+      return `${firstName.value} ${lastName.value}`;
+    },
+    //用于反向输入全名分割名与姓
+    set(newValue) {
+      const parts = newValue.split(' ');
+      firstName.value = parts[0] || '';
+      lastName.value = parts[1] || '';
+    }
+  });
+  </script>
