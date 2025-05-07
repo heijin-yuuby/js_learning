d8b8024853928182ba74c3bda27bb5f11a418c49
heijin-yuuby <wajilao214@gmail.com>
2025-05-07 22:03:44 +0800

    html binding template

---
+++ vue/style_binding/html_binding module.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+      <button @click="toggleActive">切换激活状态</button>
+      <button @click="toggleError">切换错误状态</button>
+      <div 
+        :class="{active: isActive, 'text-danger': hasError}" 
+        :style="{ fontSize: fontSize + 'px', color: textColor }"
+        class="box"
+      >
+            示例文本
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import { ref } from 'vue';
+  //设置默认值
+  export default {
+    setup() {
+      const isActive = ref(true);
+      const hasError = ref(false);
+      const fontSize = ref(16);
+      const textColor = ref('black');
+  
+      const toggleActive = () => {
+        isActive.value =!isActive.value;
+      };
+  
+      const toggleError = () => {
+        hasError.value =!hasError.value;
+      };
+  
+      return {
+        isActive,
+        hasError,
+        fontSize,
+        textColor,
+        toggleActive,
+        toggleError
+      };
+    }
+  };
+  </script>
+  
+  <style>
+  .active {
+    background-color: lightgray;
+  }
+  
+  .text-danger {
+    border: 1px solid red;
+  }
+  
+  .box {
+    padding: 10px;
+    margin-top: 10px;
+  }
+  </style>
