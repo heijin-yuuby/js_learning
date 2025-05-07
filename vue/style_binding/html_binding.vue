bb09db7d5314397b878854748bf5a541deadd383
heijin-yuuby <wajilao214@gmail.com>
2025-05-07 21:57:52 +0800

    html binding

---
+++ vue/style_binding/html_binding.vue
@@ -0,0 +1,10 @@
+<script>
+const isActive = ref(true)
+const hasError = ref(false)//这个值为true时，text-danger 会被移除   
+</script>
+<template>
+    <div 
+    
+    class="static"
+    :class="{active: isActive, 'text-danger': hasError}" ></div>
+</template>
